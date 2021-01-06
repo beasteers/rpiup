30cd7000cf970899abdc34bf5204095ece945702
Bea <bea.steers@gmail.com>
2021-01-06 11:13:30 -0500

    make boot-files available for install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ NAME = 'rpiup'
 
 setuptools.setup(
     name=NAME,
-    version='0.0.1',
+    version='0.0.2',
     description='',
     long_description=open('README.md').read().strip(),
     long_description_content_type='text/markdown',
@@ -13,6 +13,7 @@ setuptools.setup(
     author_email='bea.steers@gmail.com',
     url='https://github.com/{}/{}'.format(USERNAME, NAME),
     packages=setuptools.find_packages(),
+    package_data={NAME: {'boot-files'}},
     entry_points={'console_scripts': ['{name}={name}:cli'.format(name=NAME)]},
     install_requires=['fire'],
     extras_require={
